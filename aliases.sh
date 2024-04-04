c1b852c0109d92e2446bedd1114944461e9c6a10
Eran Boodnero <eboodnero@gmail.com>
2024-04-04 16:16:21 -0700

    updated aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,19 +1,24 @@
-export SSH_AUTH_SOCK=~/.1password/agent.sock
-
-eval "$(starship init bash)"
-
 alias ls="eza"
-alias la="eza -la"
 alias cat="bat"
 
 alias cw="cargo watch -c -q -w src/ -x \"run\""
 alias ct="cargo watch -c -q -w tests/ -x \"test -q quick_dev -- --nocapture\""
+
 alias clippy="cargo clippy -- -W clippy::pedantic -W clippy::nursery -W clippy::unwrap_used"
 
 alias brc="nvim ~/.bashrc"
 alias sb="source ~/.bashrc"
 
-# alias kb="~/scripts/keyboard_setup.sh"
-# alias thunderstorm="mpv --loop ~/Music/thunderstorm.flac"
+alias la="eza -la"
+alias thunderstorm="mpv --loop ~/Music/thunderstorm.flac"
 
 eval "$(zoxide init --cmd cd bash)"
+
+alias ehyp="nvim ~/.config/hypr/hyprland.conf"
+alias envim="nvim ~/.config/nvim"
+alias obs='nvim ~/documents/notes/$(date +"%m-%d-%Y").md'
+
+alias ein='nvim ~/scripts/install.sh'
+alias in='sh ~/scripts/install.sh'
+
+export SSH_AUTH_SOCK=~/.1password/agent.sock
