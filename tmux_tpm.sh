05cc0d0d913d4dc22115e073c75ee77f0141e4fe
Eran Boodnero <eboodnero@gmail.com>
2024-04-05 22:25:48 +0000

    removed automating tmux plugins

--- tmux_tpm.sh
+++ tmux_tpm.sh
@@ -9,19 +9,3 @@ else
     cd ~/.config/tmux/plugins/tpm && git pull
     echo "Pulled TPM changes"
 fi
-
-tmux
-
-sleep 1
-
-# Source the tmux configuration
-tmux source-file ~/.config/tmux/tmux.conf
-
-# Start a detached tmux session, run TPM install command, and kill the session
-TMUX_SESSION="tpm_setup"
-tmux new-session -d -s $TMUX_SESSION
-tmux send-keys -t $TMUX_SESSION "C-b" I
-sleep 5 # Give some time for plugins to start installing
-tmux kill-session -t $TMUX_SESSION
-
-echo "Tmux configuration sourced and TPM plugins installation triggered."
