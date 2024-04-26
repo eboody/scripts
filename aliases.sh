a4d1cb8eeee42d041e4d5698647f338f7ad2f5cf
Eran Boodnero <eboodnero@gmail.com>
2024-04-26 07:51:05 -0700

    fixed unbalanced quote in alias.sh

--- aliases.sh
+++ aliases.sh
@@ -9,14 +9,13 @@ line_exists() {
 lines_to_add=(
     'alias ls="eza"'
     'alias cat="bat"'
-    ''
     'alias cw="cargo watch -c -q -w src/ -x \"run\""'
     'alias ct="cargo watch -c -q -w tests/ -x \"test -q quick_dev -- --nocapture\""'
     'alias clippy="cargo clippy -- -W clippy::pedantic -W clippy::nursery -W clippy::unwrap_used"'
     'alias brc="nvim ~/.bashrc"'
     'alias sb="source ~/.bashrc"'
     'alias la="eza -la"'
-    'alias tree="eza --tree"
+    'alias tree="eza --tree"'
     'alias thunderstorm="mpv --loop ~/Music/thunderstorm.flac"'
     'alias ehyp="nvim ~/.config/hypr/hyprland.conf"'
     'alias envim="nvim ~/.config/nvim"'
@@ -28,12 +27,18 @@ lines_to_add=(
     'export SSH_AUTH_SOCK=~/.1password/agent.sock'
     'alias logout="pkill -KILL -u $USER"'
     'alias rsync="rsync --exclude-from=/home/eran/.config/rsync_exclude.txt"'
-    'alias tree="tree -I target -I node_modules"'
+    'alias ptree="tree -I target -I node_modules"'
     'alias cd="z"'
     'eval "$(starship init bash)"'
     'source ~/scripts/zoxide.sh'
     'export EDITOR="nvim"'
 )
+
+
+# Function to check if a line already exists in .bashrc
+line_exists() {
+    grep -Fxq "$1" ~/.bashrc
+}
 
 # Loop through lines and add to .bashrc if they don't exist
 for line in "${lines_to_add[@]}"; do
@@ -45,4 +50,7 @@ for line in "${lines_to_add[@]}"; do
     fi
 done
 
-. $HOME/.bashrc
+# Source .bashrc to reflect changes
+source ~/.bashrc
+echo ".bashrc updated and sourced."
+
