8afeb078ed09e8c27449f95ff36913b7f3a6481f
Eran Boodnero <eboodnero@gmail.com>
2024-04-26 07:41:32 -0700

    Update: 2024-04-26 07:41:32

--- brightness_down.sh
+++ brightness_down.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Check if on a laptop (assuming the presence of a battery directory)
-if ls /sys/class/power_supply/ | grep -qi battery; then
-    # Command for laptop
+chassis_type=$(cat /sys/class/dmi/id/chassis_type)
+# Common chassis types for laptops are 9 (Laptop), 10 (Notebook), 14 (Sub Notebook)
+if [[ "$chassis_type" == 9 || "$chassis_type" == 10 || "$chassis_type" == 14 ]]; then
+    # Commands for laptop
     brightnessctl set 10%-
 else
-    # Command for desktop
+    # Commands for desktop
     ddcutil setvcp 10 - 33 --display 1
 fi
-
